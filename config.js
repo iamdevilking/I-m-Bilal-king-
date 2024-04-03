ab0968e21540cadb35a2dd7c2c74e52e10eb3c8d
iamdevilking <165913342+iamdevilking@users.noreply.github.com>
2024-04-03 19:19:29 +0500

    Update config.js

--- config.js
+++ config.js
@@ -14,9 +14,9 @@ global.owner = [
 
 //global.pairingNumber = "" //put your bot number here
 
-global.mods = ['923097045246'] 
-global.prems = ['923097045246']
-global.allowed = ['923097045246']
+global.mods = ['923170460381'] 
+global.prems = ['923170460381']
+global.allowed = ['923170460381']
 global.keysZens = ['c2459db922', '37CC845916', '6fb0eff124']
 global.keysxxx = keysZens[Math.floor(keysZens.length * Math.random())]
 global.keysxteammm = ['29d4b59a4aa687ca', '5LTV57azwaid7dXfz5fzJu', 'cb15ed422c71a2fb', '5bd33b276d41d6b4', 'HIRO', 'kurrxd09', 'ebb6251cc00f9c63']
@@ -50,9 +50,9 @@ global.APIKeys = { // APIKey Here
 }
 
 // Sticker WM
-global.botname = '*˻𝐈ʈͥ᥊ ϻaͣŋͫŋ̑ο̑˼²⁴⁶-ʙᴏᴛ*'
+global.botname = '*Rajpoot brother*'
 global.premium = 'true'
-global.packname = '˻𝐈ʈͥ᥊ ϻaͣŋͫŋ̑ο̑˼²⁴⁶┃ᴮᴼᵀ' 
+global.packname = 'Rajpoot brother' 
 global.author = '.' 
 global.menuvid = 'https://i.imgur.com/8E2itvZ.mp4'
 global.igfg = '▢ Follow on Instagram\nhttps://www.instagram.com/asli_guru69\n' 
@@ -64,7 +64,7 @@ global.fglog = 'https://raw.githubusercontent.com/Guru322/api/Guru/guru.jpg'
 global.thumb = fs.readFileSync('./Assets/Gurulogo.jpg')
 
 
-global.wait = '*⌛ _Lowadind ⏜✩ ˻𝐈ʈͥ᥊ ϻaͣŋͫŋ̑ο̑˼ 𓃮•:)..._*\n*▰▰▰▰▰▱▱*'
+global.wait = '*⌛ _Lowadind ⏜✩ ˻Rajpoot brother 𓃮•:)..._*\n*▰▰▰▰▰▱▱*'
 global.rwait = '⌛'
 global.dmoji = '🤭'
 global.done = '✅'
